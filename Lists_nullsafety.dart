a976d59ac4be8f0c268491bd9628a525e6cfe8fa
Kaio Fontenele <mamakaiofrep@gmail.com>
2023-08-23 17:22:37 -0300

    Att

--- Lists_nullsafety.dart
+++ Lists_nullsafety.dart
@@ -1,1 +1,13 @@
-  void main(){}
+  void main(){
+
+ List <String> nome_cliente = ['kiwada' , 'Marcela ' , 'Lucas'];
+ print(nome_cliente);
+
+ List <String> Especial = [];
+
+ for(String cliente in nome_cliente.sublist(2)){
+  Especial.add(cliente);
+ }
+
+ print(Especial);
+  }
