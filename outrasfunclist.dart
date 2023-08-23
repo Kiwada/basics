9a89c0d6815a362882a59137acc9556756a3feb6
Kaio Fontenele <mamakaiofrep@gmail.com>
2023-08-23 17:47:00 -0300

    func lists

--- outrasfunclist.dart
+++ outrasfunclist.dart
@@ -3,7 +3,19 @@ void main(){
 
   List<int> doida = List.generate(10,(i) => i *10);
 
+ doida.removeAt(0);
 
+  print(doida.any((i) => i % 33 == 0 ));
 
-  print(doida.isEmpty);
+  print(doida.firstWhere((i) => i % 40 == 0 ));
+
+  print(doida.lastWhere((i) => i % 40 == 0 ));
+
+  print(doida.where((i) => i % 2 == 0));
+
+  print(doida.map((e) => e + 1 ));
+
+  print(doida.map((i) {return 2 * i ;
+  
+  }));
 }
